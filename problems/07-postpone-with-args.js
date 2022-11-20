da6d71290ed40bf8bec2440b88fd3811b03d296f
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-11-20 14:00:40 +0000

    prob 7

--- problems/07-postpone-with-args.js
+++ problems/07-postpone-with-args.js
@@ -24,8 +24,20 @@ slowPrintSum(2, 8); // prints '10' after 500 ms
 
 function postponeWithArgs(cb, delay) {
   // Your code here
+  return (...args) =>{
+    
+    let callback = () =>{
+      return cb(...args);
+    }
+    
+    setTimeout(callback, delay);
+  }
 }
 
+const greet = (person) => console.log('Hello ' + person + '!');
+const slowGreet = postponeWithArgs(greet, 1000);
+slowGreet('Rose'); // prints 'Hello Rose!' after 1000 ms
+slowGreet('Alex'); // prints 'Hello Alex!' after 1000 ms
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = postponeWithArgs;
